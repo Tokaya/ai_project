05da9f0db16bac58013c251b782f2cdfc424db2a
cai <jack_czm@vip.sina.com>
2017-03-24 14:16:07 +0800

    add mark and report model

--- models/image.py
+++ models/image.py
@@ -2,6 +2,30 @@ from . import *
 
 
 class Image(db.Document, ModelMixin):
-    name = db.StringField(requid=True)
-    path = db.StringField(requid=True)
+    """
+    数字病理图像
+    """
+    name = db.StringField(required=True)
+    path = db.StringField(required=True)
+    mark = db.ListField(db.EmbeddedDocumentField('mark'))
+    report = db.ListField(db.EmbeddedDocumentField('report'))
     created_time = db.DateTimeField(default=datetime.datetime.now())
+
+# created by Cai
+
+
+class Mark(db.Document, ModelMixin):
+    """
+    图像标记
+    """
+    created_time = db.DateTimeField(default=datetime.datetime.now())
+    update_time = db.DateTimeField(default=datetime.datetime.now())
+
+
+class Report(db.Document, ModelMixin):
+    """
+    病理报告
+    """
+    text = db.StringField(required=True)
+    created_time = db.DateTimeField(default=datetime.datetime.now())
+    update_time = db.DateTimeField(default=datetime.datetime.now())
